eae9753199ba2b6fdcc198084ceeca4c51eae0d2
Tales Maurício <talesmauricioraulino@gmail.com>
2017-03-24 19:10:30 -0300

    restante

---
+++ arquivos segunda semana/questao_02.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main ()
+{
+	char nome[21];
+		printf("digite seu nome:");
+			scanf("%s", nome);
+		printf("\n");
+		printf("Ola %s\n", nome);
+	return 0;
+
+}
